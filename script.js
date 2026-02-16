d74c3f6bfe4aebc5163be42003cefd987bc5aba3
Rahul Kr <rahulkumar8604745@gmail.com>
2026-02-16 11:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function minDate(dates) {
   //write you code here
+	let min = 
 }
 
 // Do not change the code
@@ -37,4 +38,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
